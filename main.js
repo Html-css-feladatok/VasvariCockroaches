3d05a6136b5bbd7099005ef5a7c2332ff61e1ef1
Péli Tamás <szt2peltam@vasvari.org>
2022-12-15 10:19:26 +0100

    resolved?

---
+++ main.js
@@ -0,0 +1,7 @@
+window.onload = document.body.style.overflow = "hidden";
+
+function scrolling() {
+  document.body.style.overflow = "auto";
+  //document.getElementById("fejresz").style.display = "none";
+  //document.getElementById("tartalom").style.opacity = "1";
+}
