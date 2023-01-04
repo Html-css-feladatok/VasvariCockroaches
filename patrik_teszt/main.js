47f4e26557f602be74e86057b457bf9bb2e148c6
Fricosz <halaszpatrik1234@gmail.com>
2023-01-05 00:14:56 +0100

    kapcsolat form + térkép + tooltip 576px-ig

--- patrik_teszt/main.js
+++ patrik_teszt/main.js
@@ -12,3 +12,7 @@ function scrolling()
 {
     document.body.style.overflow = "auto"
 }
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
